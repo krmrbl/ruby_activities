dd376e5642f15fabd23f9cbd15c4534e80476e06
krmrbl <koodlem@gmail.com>
2021-08-11 09:38:46 +0800

    changed syntax from SQLite to PostgreSQL

--- lesson_3/database_fundamentals_activity/1_students.sql
+++ lesson_3/database_fundamentals_activity/1_students.sql
@@ -2,46 +2,46 @@
 
 CREATE TABLE students (
   id integer PRIMARY KEY,
-  first_name character NOT NULL,
-  middle_name character,
-  last_name character NOT NULL,
+  first_name character varying(255) NOT NULL,
+  middle_name character varying(255),
+  last_name character varying(255) NOT NULL,
   age integer NOT NULL,
-  location character NOT NULL
+  location character varying(255) NOT NULL
 );
 
 
 -- insert 6 records:
 -- 1st:
 INSERT INTO students(id, first_name, middle_name, last_name, age, location)
-VALUES(1, "Juan", "Blank", "Cruz", 18, "Manila")
+VALUES(1, 'Juan', 'Blank', 'Cruz', 18, 'Manila')
 
 -- 2nd:
 INSERT INTO students(id, first_name, middle_name, last_name, age, location)
-VALUES(2, "Anne", "Blank", "Wall", 20, "Manila")
+VALUES(2, 'Anne', 'Blank', 'Wall', 20, 'Manila')
 
 -- 3rd:
 INSERT INTO students(id, first_name, middle_name, last_name, age, location)
-VALUES(3, "Theresa", "Blank", "Joseph", 21, "Manila")
+VALUES(3, 'Theresa', 'Blank', 'Joseph', 21, 'Manila')
 
 -- 4th:
 INSERT INTO students(id, first_name, middle_name, last_name, age, location)
-VALUES(4, "Isaac", "Blank", "Gray", 19, "Laguna")
+VALUES(4, 'Isaac', 'Blank', 'Gray', 19, 'Laguna')
 
 -- 5th:
 INSERT INTO students(id, first_name, middle_name, last_name, age, location)
-VALUES(5, "Zack", "Blank", "Matthews", 22, "Marikina")
+VALUES(5, 'Zack', 'Blank', 'Matthews', 22, 'Marikina')
 
 -- 6th:
 INSERT INTO students(id, first_name, middle_name, last_name, age, location)
-VALUES(6, "Finn", "Blank", "Lam", 25, "Manila")
+VALUES(6, 'Finn', 'Blank', 'Lam', 25, 'Manila')
 
 
 -- update first record:
-UPDATE students SET first_name = "Ivan" WHERE id = 1;
-UPDATE students SET middle_name = "Ingram" WHERE id = 1;
-UPDATE students SET last_name = "Howard" WHERE id = 1;
+UPDATE students SET first_name = 'Ivan' WHERE id = 1;
+UPDATE students SET middle_name = 'Ingram' WHERE id = 1;
+UPDATE students SET last_name = 'Howard' WHERE id = 1;
 UPDATE students SET age = 25 WHERE id = 1;
-UPDATE students SET location = "Bulacan" WHERE id = 1;
+UPDATE students SET location = 'Bulacan' WHERE id = 1;
 
 
 -- delete last record:
--- lesson_3/database_fundamentals_activity/2_students_cont.sql
+++ lesson_3/database_fundamentals_activity/2_students_cont.sql
@@ -2,7 +2,7 @@
 SELECT COUNT(id) FROM students;
 
 -- select all students with `location` is `Manila`:
-SELECT * from students GROUP BY id HAVING location == "Manila";
+SELECT * from students GROUP BY id HAVING location = 'Manila';
 
 -- display the average `age` of all `students`:
 SELECT AVG(age) from students;
